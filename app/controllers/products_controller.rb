d16d0151bf51c0fc474cab4d38b2019b3ccba5cc
Emily <emily.a.harber@gmail.com>
2020-01-06 18:40:28 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,15 +2,15 @@ class ProductsController < ApplicationController
 
   def index
     if cart.count > 0
-      @cart = cart
+      @current_cart = cart
     else 
-      @cart = ['Your Cart is Empty']
+      @current_cart = ['Your Cart is Empty']
     end 
   end 
 
   def create 
     cart << params[:product]
-    redirect_to products_path
+    redirect_to root_path
   end 
 
 end
