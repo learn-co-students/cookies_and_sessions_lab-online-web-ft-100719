69932202953d1804063528d3478d41c986c659de
Victoria Hale <vic.hale74@gmail.com>
2020-01-06 13:28:45 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def cart 
-    @cart = session[:cart] ||= []
-    @cart
+    session[:cart] ||= []
+    
   end 
 end
