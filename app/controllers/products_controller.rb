75dcbfdbf7f62aec5ba59a570e6429165c0e4dbe
rilemoore <rilemoore@gmail.com>
2020-01-10 14:33:44 -0800

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,14 @@
+
+
+class ProductsController < ApplicationController
+    helper_method :cart
+    def index
+        cart
+    end
+
+    def add
+        cart << params[:product]
+        render :index 
+    end
+
+end
